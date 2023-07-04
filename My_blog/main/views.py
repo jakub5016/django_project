c6472d509108e5f7c0ee9bb914a805c2365d6587
Jaqobel <me.jakub.gil@gmail.com>
2023-07-04 18:19:05 +0200

    Adding DB content to admin dashboard

--- My_blog/main/views.py
+++ My_blog/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 # Create your views here.
 
 def index(response, id):
-    return HttpResponse("<h1>%s</h1>" %id)
+    ls = ToDoList.objects.get(id=id)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><p>%s</p>" %(ls.name, str(item.text)))
