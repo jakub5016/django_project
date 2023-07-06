f9400d6ef7a35c5cd485d1e497153dcc72836398
Jaqobel <me.jakub.gil@gmail.com>
2023-07-06 16:20:35 +0200

    Forms

--- My_blog/main/views.py
+++ My_blog/main/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .froms import createNewList
 # Create your views here.
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.get(id=1)
     return render(response, "main/list.html", {"ls" :ls})
 
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        formula = createNewList(response.POST)
+
+        if formula.is_valid():
+            name = formula.cleaned_data["name"]
+            t = ToDoList(name=name)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id) 
+    else:
+        formula = createNewList()
+    return render(response, "main/create.html", {"form":formula})
