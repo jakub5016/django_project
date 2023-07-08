501d61c1dfa67367b3f7538cb893ed28800e65a5
Jaqobel <me.jakub.gil@gmail.com>
2023-07-08 16:06:16 +0200

    More bootstrap

--- My_blog/main/froms.py
+++ My_blog/main/froms.py
@@ -1,4 +1,11 @@
 from django import forms
+from .models import ToDoList
 
-class createNewList(forms.Form):
-    name = forms.CharField(label="name", max_length=200)
+class createNewList(forms.ModelForm):
+    class Meta:
+        model = ToDoList
+        fields = ('name', )
+
+        widget ={
+            'class': 'form-control rounded-pill w-25 bg-dark' , 'style' :'margin-bottom: 10px;margin-top: 10px; font-family:Arial;'
+        }
