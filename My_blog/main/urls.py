6ef6b0bfbda36aa6bc1d1773d0991a3eb0bc410d
Jaqobel <me.jakub.gil@gmail.com>
2023-07-08 14:16:01 +0200

    Adding Bootstrap

--- My_blog/main/urls.py
+++ My_blog/main/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("<int:id>", views.index, name="index"),
+    path("<int:id>", views.list, name="list"),
     path("", views.home, name="home"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("index/", views.index, name="index"),
 ]
--- My_blog/main/views.py
+++ My_blog/main/views.py
@@ -4,9 +4,10 @@ from .models import ToDoList, Item
 from .froms import createNewList
 # Create your views here.
 
-def index(response, id):
+all_lists = ToDoList.objects.all()
+
+def list(response, id):
     ls = ToDoList.objects.get(id=id)
-    
     if response.method =="POST":
         if response.POST.get("save"):
             for item in ls.item_set.all():
@@ -24,14 +25,16 @@ def index(response, id):
                 ls.item_set.create(text=txt, compile=False)
             else:
                 print("INVALID")
+    
 
-    return render(response, "main/list.html", {"ls" :ls})
+    return render(response, "main/list.html", {"ls":ls, "all_lists" : all_lists})
 
 
 def home(response):
-    return render(response, "main/home.html", {})
+    return render(response, "main/home.html", {"all_lists" : all_lists})
 
 def create(response):
+
     if response.method == "POST":
         formula = createNewList(response.POST)
 
@@ -43,4 +46,7 @@ def create(response):
         return HttpResponseRedirect("/%i" %t.id) 
     else:
         formula = createNewList()
-    return render(response, "main/create.html", {"form":formula})
+    return render(response, "main/create.html", {"form":formula, "all_lists" : all_lists})
+
+def index(response):
+    return render(response, "main/index.html", {"all_lists" : all_lists})
